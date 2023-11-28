d82717851853ecada9db17d5ade5c95e30c98741
algife <algife@gmail.com>
2023-11-29 00:36:21 +0100

    Creating Navbar and using it from the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Z-Linguist | Gen-Z slang translator for marketing teams.",
@@ -10,14 +11,19 @@ export const metadata: Metadata = {
     "Targeting a Gen-Z audience? Flex with your marketing game by matching their talk and vibing with their squad 💬🚀 #YoLo.",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <section className="background-radial-gradient mb-40 overflow-hidden">
+          <Navbar />
+          {children}
+        </section>
+      </body>
     </html>
-  )
+  );
 }
---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,100 @@
+import "./navbar.module.scss";
+
+export default function Navbar() {
+  return (
+    <nav
+      className="relative flex w-full items-center justify-between bg-white py-2 shadow-sm shadow-neutral-700/10 dark:bg-neutral-800 dark:shadow-black/30  lg:flex-wrap lg:justify-start"
+      data-te-navbar-ref
+    >
+      <div className="flex w-full flex-wrap items-center justify-between px-6">
+        <div className="flex items-center">
+          <button
+            className="block border-0 bg-transparent py-2 pr-2.5 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
+            type="button"
+            data-te-collapse-init
+            data-te-target="#navbarSupportedContentY"
+            aria-controls="navbarSupportedContentY"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="[&>svg]:w-7">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-7 w-7"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </button>
+
+          {/* <!-- Navbar Brand --> */}
+          <a className="text-primary dark:text-primary-400" href="#!">
+            <span className="[&>svg]:ml-2 [&>svg]:mr-3 [&>svg]:h-6 [&>svg]:w-6 [&>svg]:lg:ml-0">
+              Z-Linguist
+            </span>
+          </a>
+        </div>
+
+        {/*  <div
+          className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
+          id="navbarSupportedContentY"
+          data-te-collapse-item
+        >
+         <ul className="mr-auto lg:flex lg:flex-row" data-te-navbar-nav-ref>
+            <li data-te-nav-item-ref>
+              <a
+                className="block py-2 pr-2 text-neutral-500 transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
+                href="#!"
+                data-te-nav-link-ref
+                data-te-ripple-init
+                data-te-ripple-color="light"
+                // disabled={true}
+              >
+                Dashboard
+              </a>
+            </li>
+            <li data-te-nav-item-ref>
+              <a
+                className="block py-2 pr-2 text-neutral-500 transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
+                href="#!"
+                data-te-nav-link-ref
+                data-te-ripple-init
+                data-te-ripple-color="light"
+              >
+                Team
+              </a>
+            </li>
+            <li className="mb-2 lg:mb-0" data-te-nav-item-ref>
+              <a
+                className="block py-2 pr-2 text-neutral-500 transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
+                href="#!"
+                data-te-nav-link-ref
+                data-te-ripple-init
+                data-te-ripple-color="light"
+              >
+                Projects
+              </a>
+            </li>
+          </ul>
+        </div> */}
+
+        {/* <div className="my-1 flex items-center lg:my-0 lg:ml-auto">
+          <button
+            type="button"
+            className="mr-2 inline-block rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 dark:text-primary-400 dark:hover:bg-neutral-700 dark:hover:bg-opacity-60 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+            data-te-ripple-init
+            data-te-ripple-color="light"
+          >
+            Login
+          </button>
+        </div> */}
+      </div>
+    </nav>
+  );
+}
